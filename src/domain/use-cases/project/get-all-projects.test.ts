d83430c2bc75637d2f1c76dccd148ab372853322
Daniel Vinicius <viniccius774@gmail.com>
2024-01-10 09:28:14 -0300

    test: get all projects usecase test

---
+++ src/domain/use-cases/project/get-all-projects.test.ts
@@ -0,0 +1,44 @@
+import { Project } from "../../entities/project";
+import { ProjectRepository } from "../../interfaces/repositories/project-repository";
+import { GetAllProjects } from "./get-all-projects";
+
+describe("Get all projects Use Case", () => {
+  class MockProjectRepository implements ProjectRepository {
+    getProject(id: string): Promise<Project> {
+      throw new Error("Method not implemented.");
+    }
+    getAllProjects(): Promise<Project[]> {
+      throw new Error("Method not implemented.");
+    }
+    createProject(project: Project): Promise<void> {
+      throw new Error("Method not implemented.");
+    }
+    updateProject(id: string, project: Project): Promise<void> {
+      throw new Error("Method not implemented.");
+    }
+    deleteProject(id: string): Promise<void> {
+      throw new Error("Method not implemented.");
+    }
+  }
+
+  let mockProjectRepository: MockProjectRepository;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockProjectRepository = new MockProjectRepository();
+  });
+
+  it("should return data", async () => {
+    const expected: Project[] = [
+      { id: "1", name: "Project alpha", description: "The alpha project" },
+    ];
+
+    jest.spyOn(mockProjectRepository, "getAllProjects").mockResolvedValue(expected);
+
+    const getAllProjectsUseCase = new GetAllProjects(mockProjectRepository);
+    const result = await getAllProjectsUseCase.execute();
+
+    expect(mockProjectRepository.getAllProjects).toHaveBeenCalledTimes(1);
+    expect(result).toStrictEqual(expected);
+  });
+});
