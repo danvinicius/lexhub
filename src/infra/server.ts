0e5ecf147fd0c67bc87566f5502aa264f90c6cf1
Daniel Vinicius <viniccius774@gmail.com>
2024-04-13 20:01:39 -0300

    chore: add helmet

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import helmet from 'helmet';
 const server = express();
+server.use(helmet())
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 export default server;
