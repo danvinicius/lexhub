ef5d9ef83e6cb7e7f1daff5dfbf31d32d9b230c2
Daniel Vinícius <viniccius774@gmail.com>
2024-12-26 12:42:34 -0300

    chore: fix mongo connection url string

--- api/src/infra/db/connection.ts
+++ api/src/infra/db/connection.ts
@@ -3,6 +3,6 @@ import mongoose, { Mongoose } from 'mongoose';
 const {MONGO_ROOT_USERNAME, MONGO_ROOT_PASSWORD, MONGO_HOST, MONGO_NAME, MONGO_PORT} = process.env;
 
 export const connect = async (): Promise<Mongoose> =>
-  await mongoose.connect(`mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_NAME}`);
+  await mongoose.connect(`mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_NAME}?authSource=admin`);
 
 export const close = (): Promise<void> => mongoose.connection.close();
